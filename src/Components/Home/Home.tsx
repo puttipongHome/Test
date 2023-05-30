d951edb0259aaed945f2e6a5919c6e37ec4076a9
Home <homehome12589@hotmail.com>
2023-05-30 12:44:23 +0700

    update

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -3,7 +3,60 @@ import { Card } from "antd";
 import { Link } from "react-router-dom";
 import "./Home.css";
 import { useTranslation } from "react-i18next";
+import axios from "axios";
 
+type GetResponse = {
+  text: string;
+};
+async function getUsers() {
+  try {
+    const { data } = await axios.post<GetResponse>("/api/sub", {
+      text: ["BATHROOM", "BATHSALTS", "BLOODBATH"],
+      subString: ["BATH"],
+    });
+
+    return data;
+  } catch (error: any) {
+    return error.message;
+  }
+}
+getUsers();
+
+async function getClock() {
+  try {
+    const { data } = await axios.post<GetResponse>("/api/clock", {
+      text: "18:00",
+    });
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message: ", error.message);
+      return error.message;
+    } else {
+      console.log("unexpected error: ", error);
+      return "An unexpected error occurred";
+    }
+  }
+}
+getClock();
+async function getCard() {
+  try {
+    const { data } = await axios.post<GetResponse>("/api/pocker", {
+      text: "S8 S10 CA",
+    });
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message: ", error.message);
+      return error.message;
+    } else {
+      console.log("unexpected error: ", error);
+      return "An unexpected error occurred";
+    }
+  }
+}
+
+getCard();
 function Home() {
   const { t } = useTranslation(["home", "main"]);
   return (
